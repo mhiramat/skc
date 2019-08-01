d2ff6e8840969d9698930810f9f35550c393355e
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2019-08-02 00:51:06 +0900

    skc: Cleanup file loading code
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@gmail.com>

--- main.c
+++ main.c
@@ -8,32 +8,46 @@
 #include "compat.h"
 #include "skc.h"
 
-int main(int argc, char **argv)
+/* Return the read size or -errno */
+int load_skc_file(const char *path, char **buf)
 {
 	struct stat stat;
 	int fd, ret;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return -errno;
+	ret = fstat(fd, &stat);
+	if (ret < 0)
+		return -errno;
+
+	*buf = malloc(stat.st_size + 1);
+	if (!*buf)
+		return -ENOMEM;
+
+	ret = read(fd, *buf, stat.st_size);
+	if (ret < 0)
+		return -errno;
+	(*buf)[ret] = '\0';
+
+	close(fd);
+
+	return ret;
+}
+
+int main(int argc, char **argv)
+{
 	char *buf;
+	int ret;
 
 	if (argc != 2)
 		return -1;
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-		return fd;
-	ret = fstat(fd, &stat);
-	if (ret < 0)
+	ret = load_skc_file(argv[1], &buf);
+	if (ret < 0) {
+		printf("Failed to load %s : %d\n", argv[1], ret);
 		return ret;
-
-	buf = malloc(stat.st_size + 1);
-	if (!buf)
-		return -ENOMEM;
-
-	ret = read(fd, buf, stat.st_size);
-	if (ret < 0)
-		return ret;
-	buf[ret] = '\0';
-
-	close(fd);
+	}
 
 	ret = skc_parse(buf, ret);
 
